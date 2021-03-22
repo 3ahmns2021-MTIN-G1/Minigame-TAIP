8983da6aea46a363503fb17f719e10184503c991
Peter Taitl <peter.taitl@icloud.com>
2021-03-22 13:26:14 +0100

    Player Movement hinzugefügt

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+
+    public Rigidbody rb;
+    public string links;
+    public string rechts;
+    public string oben;
+    public int moveForce;
+    public int jumpForce;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (Input.GetKeyDown(links))
+        {
+            rb.AddForce(new Vector3(-moveForce, 0, 0),ForceMode.Force);
+           
+        }
+
+        else if (Input.GetKeyDown(rechts))
+        {
+            rb.AddForce(new Vector3(moveForce, 0, 0), ForceMode.Force);
+          
+        }
+
+        else if (Input.GetKeyDown(oben))
+        {
+            rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Force);
+      
+        }
+
     }
 }
